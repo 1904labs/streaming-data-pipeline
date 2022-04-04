dede91aa255f7e3ef2fe2aa11bc739930a8fd5fa
Nick Rafferty <nickrafferty78@gmail.com>
2022-04-04 17:03:47 -0500

    Added homework for Spark

--- spark-hello-world/src/main/scala/com/labs1904/hwe/homework/HelloWorldBatchApp.scala
+++ spark-hello-world/src/main/scala/com/labs1904/hwe/homework/HelloWorldBatchApp.scala
@@ -1,4 +1,4 @@
-package com.labs1904.hwe
+package com.labs1904.hwe.homework
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.{Dataset, SparkSession}
@@ -10,10 +10,13 @@ object HelloWorldBatchApp {
   def main(args: Array[String]): Unit = {
     try {
       logger.info(s"$jobName starting...")
+      //TODO: What is a spark session - Why do we need this line?
       val spark = SparkSession.builder()
         .appName(jobName)
         .config("spark.sql.shuffle.partitions", "3")
+        //TODO- What is local[*] doing here?
         .master("local[*]")
+        //TODO- What does Get or Create do?
         .getOrCreate()
 
       import spark.implicits._
@@ -21,6 +24,7 @@ object HelloWorldBatchApp {
       // print out the names and types of each column in the dataset
       sentences.printSchema
       // display some data in the console, useful for debugging
+      //TODO- Make sure this runs successfully
       sentences.show(truncate = false)
     } catch {
       case e: Exception => logger.error(s"$jobName error in main", e)
