af83ea59709fb16aa79ea18b2d9d815a7900034f
Kit Menke <cwmenke@gmail.com>
2022-04-25 17:42:10 -0500

    Add properties for secure kafka.

--- streaming-data-pipeline/src/test/scala/com/labs1904/spark/StreamingPipelineTests.scala
+++ streaming-data-pipeline/src/test/scala/com/labs1904/spark/StreamingPipelineTests.scala
@@ -1,6 +1,8 @@
 package com.labs1904.spark
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
@@ -8,5 +10,13 @@ import org.scalatest.FunSuite
 
 
 class StreamingPipelineTests extends FunSuite with DataFrameSuiteBase {
-
+  test("files were written to our user directory") {
+    val config = new Configuration()
+    config.set("dfs.client.use.datanode.hostname", "true")
+    config.set("fs.defaultFS", StreamingPipeline.hdfsUrl)
+    val filesystem = FileSystem.get(config)
+    val fileStatuses = filesystem.listStatus(new Path(s"/user/${StreamingPipeline.hdfsUsername}/reviews_json/"))
+    fileStatuses.foreach(println)
+    assert(fileStatuses.length > 0)
+  }
 }
