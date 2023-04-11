bfbd3db538d23096fae6bc61ddc20199876b69a1
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-04-10 19:13:33 -0500

    ConnectionTest success.

--- kafka-hello-world/src/main/scala/com/labs1904/hwe/consumers/ConnectionTest.scala
+++ kafka-hello-world/src/main/scala/com/labs1904/hwe/consumers/ConnectionTest.scala
@@ -1,4 +1,4 @@
-package come.labs1904.hwe.consumers
+package com.labs1904.hwe.consumers
 
 import com.labs1904.hwe.util.Constants._
 import com.labs1904.hwe.util.Util
@@ -10,19 +10,7 @@ import java.time.Duration
 import java.util.Arrays
 
 object ConnectionTest {
-<<<<<<< HEAD
-  // TODO: this is configured to use kafka running locally, change it to your cluster
-  val BootstrapServer : String = "b-3-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-2-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-1-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196"
-  val Topic: String = "connection-test"
-  val username: String = "1904labs"
-  val password: String = "1904labs"
-  //Use this for Windows
-//  val trustStore: String = "src\\main\\resources\\kafka.client.truststore.jks"
-  //Use this for Mac
-  val trustStore: String = "src/main/resources/kafka.client.truststore.jks"
 
-=======
->>>>>>> d7350b9196343e04a94c1ffdb9d477ddabf34157
   implicit val formats: DefaultFormats.type = DefaultFormats
   private val logger = LoggerFactory.getLogger(getClass)
 
--- kafka-hello-world/src/main/scala/com/labs1904/hwe/util/Constants.scala
+++ kafka-hello-world/src/main/scala/com/labs1904/hwe/util/Constants.scala
@@ -4,10 +4,10 @@ import java.nio.file.Paths
 
 object Constants {
   // TODO: this is configured to use kafka running locally, change it to your cluster
-  val BOOTSTRAP_SERVER : String = "CHANGEME"
-  val DEFAULT_TOPIC: String = "hwe-kafka-connection-test"
-  val USERNAME: String = "CHANGEME"
-  val PASSWORD: String = "CHANGEME"
+  val BOOTSTRAP_SERVER : String = "b-3-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-2-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-1-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196"
+  val DEFAULT_TOPIC: String = "connection-test"
+  val USERNAME: String = "1904labs"
+  val PASSWORD: String = "1904labs"
   //Use this for Windows
   val TRUST_STORE: String = {
     val url = getClass.getResource("/kafka.client.truststore.jks")
