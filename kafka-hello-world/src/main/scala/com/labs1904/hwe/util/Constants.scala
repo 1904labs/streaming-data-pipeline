19804c8db81774ceb31c802d3fc2212a982fc12c
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-05-03 14:19:32 -0500

    Removed bootstrap.

--- kafka-hello-world/src/main/scala/com/labs1904/hwe/util/Constants.scala
+++ kafka-hello-world/src/main/scala/com/labs1904/hwe/util/Constants.scala
@@ -4,8 +4,8 @@ import java.nio.file.Paths
 
 object Constants {
   // TODO: this is configured to use kafka running locally, change it to your cluster
-  val BOOTSTRAP_SERVER : String = "b-3-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-2-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196,b-1-public.hwekafkacluster.6d7yau.c16.kafka.us-east-1.amazonaws.com:9196"
-  val DEFAULT_TOPIC: String = "connection-test"
+  val BOOTSTRAP_SERVER : String = "CHANGE ME"
+  val DEFAULT_TOPIC: String = "hwe-kafka-connection-test"
   val USERNAME: String = "1904labs"
   val PASSWORD: String = "1904labs"
   //Use this for Windows
